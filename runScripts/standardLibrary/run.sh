088c85a0a4dbe33b59a2df71add36bb3acb2649f
TomCrow <tomcrow@vbox.fritz.box>
2025-09-16 18:45:59 +0200

    feat: experimented with the gem5 standard library

--- runScripts/standardLibrary/run.sh
+++ runScripts/standardLibrary/run.sh
@@ -4,6 +4,6 @@ GEM5_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../gem5 && pwd)"
 cd "$SCRIPT_DIR" || exit 1
 
 # Change out this script file name
-SCRIPT_NAME="runLinux.py"
+SCRIPT_NAME="standardLibrary.py"
 
 "$GEM5_DIR"/build/ALL/gem5.opt "$SCRIPT_NAME"
